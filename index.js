e8631707a22b4cd0205b078ebc38c2c18912c06f
Fabiano Cunha <cunhasb@aol.com>
2018-02-22 22:50:57 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function buildHistogram(magazine) {
+  let dictionary = {};
+  for (let i in magazine) {
+    dictionary[magazine[i]]
+      ? (dictionary[magazine[i]] += 1)
+      : (dictionary[magazine[i]] = 1);
+  }
+  return dictionary;
+}
+
+function canBuildNote(magazine, note) {
+  let result = true;
+  let magazineDict = buildHistogram(magazine);
+  let noteDict = buildHistogram(note);
+  for (let i in noteDict) {
+    if (!magazineDict[i]) {
+      return false;
+    }
+    if (noteDict[i] > magazineDict[i]) {
+      return false;
+    }
+  }
+  return true;
+}
